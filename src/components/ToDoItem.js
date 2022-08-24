d2069313340efa6ee2f89ef616a2840da63b313d
Terry Babyuk <tbabyuk@gmail.com>
2022-08-24 00:06:14 -0400

    changed the handling of todo item status to a more react-like approach

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,27 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ToDoItem({ task, date, id, handleDelete }) {
 
-  const handleStatus = (e) => {
-    console.log(e.target.parentElement.parentElement.id)
-    if(e.target.value === 'not started') {
-      e.target.parentElement.parentElement.id = 'todo-not-started'
-    } else if(e.target.value === 'in progress') {
-      e.target.parentElement.parentElement.id = 'todo-in-progress'
-    } else if(e.target.value === 'completed') {
-      e.target.parentElement.parentElement.id = 'todo-completed'
-  }
-}
+  const [status, setStatus] = useState('not-started');
 
+  
   return (
-      <li className="list-group-item mb-1 pe-5 d-flex justify-content-between align-items-center rounded" id="todo-not-started">
+      <li className="list-group-item mb-1 pe-5 d-flex justify-content-between align-items-center rounded" id={status}>
             <span className='delete-todo-button' onClick={() => handleDelete(id)}>&times;</span>
             <span className='task-text fs-5'>{task}</span>
             <div>
               <small>Due date: {date}</small>
-              <select className="form-select form-select-sm mt-2" onChange={handleStatus}>
-                <option value='not started'>not started</option>
-                <option value='in progress'>in progress</option>
+              <select className="form-select form-select-sm mt-2" onChange={(e) => setStatus(e.target.value)}>
+                <option value='not-started'>not started</option>
+                <option value='in-progress'>in progress</option>
                 <option value='completed'>completed</option>
               </select>
             </div>
